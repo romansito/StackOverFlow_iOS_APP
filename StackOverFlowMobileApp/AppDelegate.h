03697c68cd6ea295a4057b732f4355b61a2fb400
Roman Salazar Lopez <romansito18@gmail.com>
2015-12-07 13:57:18 -0800

    Initial Commit

---
+++ StackOverFlowMobileApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackOverFlowMobileApp
+//
+//  Created by Roman Salazar Lopez on 12/7/15.
+//  Copyright © 2015 Roman Salazar Lopez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
